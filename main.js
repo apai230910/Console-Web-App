1fe583b23781a8272ca2d64cfee864e52996b49c
apai230910 <91840707+apai230910@users.noreply.github.com>
2021-12-19 09:55:16 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,43 @@
+
+    console.log("My favourite superhero is Thor");
+
+    var first_name = "Thor ";
+    console.log(first_name);
+
+    var last_name = "Odinson";
+    console.log(last_name);
+
+    var full_name = first_name.concat(last_name);
+    console.log(full_name);
+
+    var length = full_name.length;
+    console.log(length);
+
+    var name = "Thor";
+    console.log(name);
+
+    var replace_name = name.replace("Thor", "Thor Odinson");
+    console.log(replace_name);
+
+    var capital_name = "THOR";
+    var making_lower_case = capital_name.toLowerCase();
+    console.log(making_lower_case);
+
+    var small_name = "thor";
+    var making_upper_case = small_name.toUpperCase();
+    console.log(making_upper_case);
+
+    var square_root = Math.sqrt(64);
+    console.log(square_root);
+
+    var maximum_number = Math.max(23, 965);
+    console.log(maximum_number);
+
+    var minimum_number = Math.min(23, 965);
+    console.log(minimum_number);
+
+    var power_eg_1 = Math.pow(13, 4);
+    console.log(power_eg_1);
+
+    var power_eg_2 = Math.pow(34, 3);
+    console.log(power_eg_2);
